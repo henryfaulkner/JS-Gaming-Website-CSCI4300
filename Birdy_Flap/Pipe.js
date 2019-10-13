33539a65227bd2d048cd8f057084c1d7f70f31f3
Henry Faulkner <hfaulkner27@gmail.com>
2019-10-13 17:32:52 -0400

    added RegEx is account; added Flappy Bird

---
+++ Birdy_Flap/Pipe.js
@@ -0,0 +1,40 @@
+/**
+  Pipe Constructor
+*/
+function Pipe() {
+  this.top = random(height-80); 
+  this.gap = 80; //gap btw top & bottom pipe
+  this.x = width; //starting point
+  this.w = 20; //pipe width
+  this.speed = 5; //pipe speed
+  this.highlight = false; //
+  
+  this.show = function() {
+    fill('#0D9428');
+    if(this.highlight) {
+       fill(255, 0, 0);
+    }
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, this.top + this.gap, this.w, 500);
+  }
+  
+  this.update = function() {
+    this.x -= this.speed; 
+  }
+  
+  this.offscreen = function() {
+     return this.x < -this.w;  
+  }
+  
+  this.hits = function(bird) {
+    //within y-gap
+    if(bird.y < this.top || bird.y > this.top + this.gap){
+      //within x-position
+      if(bird.x > this.x && bird.x < this.x + this.w){
+        this.highlight = true;
+        return true; 
+      } 
+    } 
+    return false;
+  }
+}
---
+++ Birdy_Flap/sketch.js
@@ -0,0 +1,65 @@
+var bird;
+var pipes = [];
+var score;
+var startLoop = false;
+
+function setup() {
+  createCanvas(400, 400);
+  bird = new Bird();
+  pipes.push(new Pipe());
+  score = 0;
+}
+
+function draw() {
+  //waits for user to press before starting game
+  if(startLoop == false) {
+    noLoop();
+  }
+  background('#A3F7FA');
+  for(var i=pipes.length-1; i >= 0; i--){
+    pipes[i].show();
+    pipes[i].update();
+    
+    //deals with bird to pipe collision
+    if(pipes[i].hits(bird)){
+      console.log("HIT");
+      textSize(62);
+      fill(0);
+      textAlign(CENTER);
+      textStyle(BOLD);
+      stroke(0);
+      text("Score: " + score,200,200);
+      score = 0; //resets score
+      noLoop();
+    } else if(bird.x > pipes[i].x && bird.x < pipes[i].x + pipes[i].w) {  
+      //increments & prints score if bird passes pipe
+      //& does not hit
+      score += .25; //fixes issue of multi-scoring
+      if(score % 1 == 0) {
+        console.log("Score: " + score);
+      }
+    }
+    
+    //deletes used pipes
+    if(pipes[i].offscreen()) {
+      pipes.splice(i,1); 
+    }
+  }
+  
+  bird.show();
+  bird.update();
+  
+  if(frameCount % 60 == 0) {
+    pipes.push(new Pipe());
+  }
+  
+}
+
+function keyPressed(){
+  if(key == " " && startLoop == false){
+    startLoop = true;
+    loop();
+  } else if(key == " "){
+    bird.jump(); 
+  }
+}
